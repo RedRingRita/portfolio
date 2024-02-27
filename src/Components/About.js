39cc7bbcb58b896a3a4201306c4d6b91ae615675
RedRingRita <zhavina@protonmail.com>
2024-02-27 14:54:09 +0100

    Ajout header

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import Image from 'react-bootstrap/Image';
+
+function About() {
+
+    return (
+        <Image src="../Assets/Images/prunelleEtSoleil.jpeg" alt='Prunelle sous le soleil' roundedCircle />
+    );
+}
+
+export default About;
---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark" sticky="top">
+      <Container >
+        <Navbar.Brand href="#home">Prunelle STOESSEL</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">A propos</Nav.Link>
+            <Nav.Link href="#work">Mon travail</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
